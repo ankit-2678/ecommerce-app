48384443b87d8a54838fa36b58675dfee132c6b4
ankit <ankitsinghktr4@gmail.com>
2026-04-10 19:32:59 +0530

    feat: setup backend architecture with folder structure, MongoDB and Cloudinary integration, and product & user controllers

---
+++ Backend/routes/productRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { addProduct,removeProduct,listProducts,singleProduct } from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+
+const productRouter = express.Router();
+
+productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct)
+productRouter.delete('/remove/:id',removeProduct)
+productRouter.get('/single/:id',singleProduct)
+productRouter.get('/list',listProducts)
+
+
+export default productRouter;
