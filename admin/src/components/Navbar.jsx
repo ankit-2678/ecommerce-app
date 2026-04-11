0b2f0b8b500b4779ff60889add98791d2ac9b2f3
ankit <ankitsinghktr4@gmail.com>
2026-04-11 16:38:33 +0530

    feat: implement admin authentication with login API integration, token management, and logout functionality with navbar update

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-function Navbar() {
+function Navbar({setToken}) {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded full'>Logout</button>
+      <button  onClick={()=>{localStorage.removeItem("token"); setToken('')}} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full'>Logout</button>
     </div>
   )
 }
